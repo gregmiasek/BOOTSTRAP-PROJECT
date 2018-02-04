f7875a406a9576946125f2ce10923aefb9a2b9e1
gregmiasek <gmiasek@gmail.com>
2018-02-04 16:49:01 +0100

    smooth scroll

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $('a[href*="http"]').click(function (fading) {
     var url = $(this).attr('href');
     console.log(url);
 
-    $('body').fadeOut(5000, function () {
+    $('body').fadeOut(2000, function () {
         window.location.href = url;
     });
 
@@ -31,4 +31,43 @@ $(window).scroll(function () {
     $(".progressbar").css("width", proc + "%");
 
     console.log(proc);
+});
+
+$('#submit').click(function (getinfo) {
+    getinfo.preventDefault();
+
+    var formData = []
+
+    var name = $('#name').val();
+    var mail = $('#mail').val();
+    var tel = $('#tel').val();
+    var message = $('#mess').val();
+
+    formData.push('imię: ' + name);
+    formData.push('email: ' + mail);
+    formData.push('telefon: ' + tel);
+    formData.push('treść wiadomości: ' + message);
+    console.log(message);
+
+    console.log(formData)
+
+    var formdane = JSON.stringify(formData);
+
+    console.log(formdane);
+
+})
+
+$(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
 });
